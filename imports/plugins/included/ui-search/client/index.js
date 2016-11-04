404c4cec4b1de419d0323df45c6056589ffecced
Chukwuma Ezumezu <chukwuma.ezumezu@andela.com>
2016-11-04 13:50:27 +0100

    feature(sort n filter) - rebase to develop

--- imports/plugins/included/ui-search/client/index.js
+++ imports/plugins/included/ui-search/client/index.js
@@ -13,7 +13,6 @@ import "./templates/productSearch/productItem.html";
 import "./templates/productSearch/productItem.js";
 import "./templates/productSearch/content.html";
 import "./templates/productSearch/content.js";
-import "./templates/productSearch/sortAndFilter.html";
 
 // Order Search
 import "./templates/orderSearch/orderResults.html";
