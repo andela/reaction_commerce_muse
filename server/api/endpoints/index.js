9a75a25c7379a46df90063e1534e0ef8f25dd2ee
Natalie <natalie.elizabeth@andela.com>
2017-06-05 14:11:21 +0300

    [Feature #145751723 restful api endpoints for assets

--- server/api/endpoints/index.js
+++ server/api/endpoints/index.js
@@ -1,15 +1,4 @@
+
 export * from "./accounts";
 export * from "./analytics";
-// export * from "./products";
-// export * from "./shops";
-
-
-// export * from "./assets";
-// export * from "./emails";
-// export * from "./cart";
-// export * from "./discounts";
-// export * from "./inventory";
-// export * from "./orders";
-// export * from "./revisions";
-// export * from "./tags";
-// export * from "./translations";
+export * from "./assets";
