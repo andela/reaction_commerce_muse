f16b92d001023706c16fe5f7e43d8cd0f45f190e
Natalie <natalie.elizabeth@andela.com>
2017-06-06 11:02:57 +0300

    products endpoint

---
+++ server/api/endpoints/products.js
@@ -0,0 +1,38 @@
+import { Products } from "../../../lib/collections/";
+
+export function products(Api) {
+  Api.addCollection(Products);
+  Api.addRoute("products", { authRequired: false }, {
+    get: () => {
+      return Products.find().fetch();
+    },
+    post: {
+      action: () => {
+        if (Products.insert(this.request.body)) {
+          return { status: "success", message: "Product added succesfully" };
+        }
+        return {
+          status: "fail",
+          message: "Product failed to add"
+        };
+      }
+    }
+  });
+  Api.addRoute("products/:id", { authRequired: false }, {
+    get: () => {
+      return Products.findOne().fetch();
+    },
+    delete: {
+      roleRequired: ["author", "admin"],
+      action: () => {
+        if (Products.remove(this.urlParams.id)) {
+          return { status: "success", message: "Product deleted" };
+        }
+        return {
+          status: "fail",
+          message: "Product not found"
+        };
+      }
+    }
+  });
+}
